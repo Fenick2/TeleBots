9f28c2b47c2d36d831fcacf831bc62deb8f6e652
Fenick <fenick2@yandex.ru>
2021-10-12 19:28:51 +0300

    secure fix

--- bot.py
+++ bot.py
@@ -1,25 +1,28 @@
-from misc import token
+import os
 from yobit import get_btc_eth
 from datetime import datetime
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from dotenv import load_dotenv
 
 
-bot = Bot(token=token)
+load_dotenv()
+
+bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start', 'help'])
 async def start_command(message: types.Message):
-    await message.reply('Привет!\nНапиши или нажми "/rate" и я пришлю тебе текущий курс биткоина и этериума!')
+    await message.reply('Привет!\nНапиши или нажми "/rate" и я пришлю тебе текущий курс Биткоина и Эфира!')
 
 
 @dp.message_handler(commands=['rate', 'курс'])
 async def btc_level(message: types.Message):
     try:
         await message.reply(f"***{datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
-                            f'** Текущий курс: **\n{get_btc_eth()}'
+                            f'*** Текущий курс: ***\n{get_btc_eth()}'
                             )
     except:
         await message.reply('\U00002620 Ошибочка! Повтори попозже. \U00002620')
